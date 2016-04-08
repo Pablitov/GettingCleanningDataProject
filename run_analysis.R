699d65af4cf0aeaa17e00e364f908a731fa929ed
Pablitov <pterancobo@gmail.com>
2016-04-07 17:24:51 -0700

    Final submission
    forgot to update the R file

--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,10 @@
 # -----------------------------------------------------------------------
 library(data.table)
 library(dplyr)
-library(plyr)
+library(curl)
 library(reshape2)
 
-setwd("~/R/Module3/UCI HAR Dataset")
+setwd("~/R/Module3 GCD/UCI HAR Dataset")
 wd <- getwd()
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
 download.file(fileURL, destfile = "data.zip")
@@ -18,10 +18,6 @@ unzip("data.zip")
 # "features.txt" contains the names of the variables
 features <- read.table("features.txt")
 
-# By looking at this data, we can indentify the column where the feature names are
-names(features)
-# the name of the column that we want is "V2".
-
 # Load all the data
 test_data <- read.table(paste0(wd,"/test/X_test.txt"))
 test_subject <- read.table(paste0(wd,"/test/subject_test.txt"))
@@ -29,9 +25,6 @@ names(test_subject) <- "subject"
 test_activity <- read.table(paste0(wd,"/test/y_test.txt"))
 names(test_activity) <- "activity"
 test_data <- cbind(test_subject,test_activity,test_data)
-# Now we can change the names of the test data to use them later in order to merge the two data sets
-
-# Load the subjects and activities, and then merge them to the test dataframe
 
 # Then the "train" dataset 
 train_data <- read.table(paste0(wd,"/train/X_train.txt"))
@@ -59,8 +52,8 @@ Complete_dataset <- Complete_dataset[mean_std_ind,]
 # -----------------------------------------------------------------------
 activityid <- read.table("activity_labels.txt")
 Complete_dataset <- merge(activityid, Complete_dataset, by.x="V1", by.y="activity")
-setnames(Complete_dataset,"V1","activity#")
-setnames(Complete_dataset,"V2","activityid")
+setnames(Complete_dataset,"V1","activity")
+setnames(Complete_dataset,"V2","activityname")
 
 # 4. Appropriately labels the data set with descriptive variable names
 # -----------------------------------------------------------------------
@@ -72,7 +65,8 @@ setnames(Complete_dataset,names(Complete_dataset),dsnames)
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 # -----------------------------------------------------------------------
-dsnames2 <- names(Complete_dataset)[]
-Final_data <- aggregate(Complete_dataset[dsnames2], by=Complete_dataset[c("subject","activity#")], FUN=mean)
+dsnames <- names(Complete_dataset)
+Final_data <- aggregate(Complete_dataset[dsnames], by=Complete_dataset[c("subject","activity")], FUN=mean)
+Final_data <-aggregate(. ~subject + activity, Complete_dataset, mean)
 # To end, we write the file 
-write.table(Final_data,"Final_data.txt", row.name=FALSE)
+write.table(Final_data,"Tidydata.txt", row.name=FALSE)
